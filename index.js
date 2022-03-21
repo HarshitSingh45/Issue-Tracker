acd6eff45e0b52b997cb98f0865049fdd0bfc450
Harshit Singh <harshitsingh@Ankurs-MacBook-Air.local>
2022-03-21 14:28:51 +0530

    updated

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const port =8000;
 const db = require('./config/mongoose');
+const expressLayout = require('express-ejs-layouts');
 const app = express();
+const sassMiddleware = require('node-sass-middleware');
+app.use(sassMiddleware({
+    /* Options */
+    src: './assets/scss'
+  , dest: './assets/styles'
+  , debug: true
+  , outputStyle: 'extended'
+  , prefix: '/styles'
+}));
 app.use(express.urlencoded());
-app.use(express.static('/assets'));
+app.use(express.static('./assets'));
+app.use(expressLayout);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
