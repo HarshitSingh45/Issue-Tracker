96f0e6bc0e1a765731b188b3a9cd1f7b1a8585cf
Harshit Singh <harshitsingh@Ankurs-MacBook-Air.local>
2022-03-21 13:14:39 +0530

    added models

---
+++ models/project.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    issues: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Issue'
+        }
+    ]
+},{
+    timestamps: true
+})
+
+const Project = mongoose.model('Project', projectSchema);
+module.exports = Project;
