e4cf911ab520c04b44c669ad7b1b9bd5bf0e11ce
Harshit Singh <harshitsingh@Ankurs-MacBook-Air.local>
2022-03-21 12:27:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const port =8000;
+const app = express();
+
+app.listen(port, err => {
+    if(err){ console.log('Error in running the server'); return;}
+    console.log(`Server is up and running on port: ${port}`);
+});
